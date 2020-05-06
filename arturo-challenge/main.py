169ee45f34d9c2f966b78a96954000a15e554951
Andrew Hummel <ahummel25@gmail.com>
2020-05-06 09:02:07 -0500

    Add challenge solution

---
+++ arturo-challenge/main.py
@@ -0,0 +1,10 @@
+from typing import List
+from itertools import count, filterfalse
+
+def return_smallest_positive(l: List) -> int:
+	return next(filterfalse(set(l).__contains__, count(1)))
+
+if __name__ == "__main__":
+	l = [0,14,2,5,3,7,8,12]
+	r = return_smallest_positive(l)
+	print(r)
