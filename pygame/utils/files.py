1ade6c39b95a9f8c0154da618b14973d36614706
Andrew Hummel <ahummel25@gmail.com>
2021-02-10 13:43:47 -0600

    Add return type to function

--- pygame/utils/files.py
+++ pygame/utils/files.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 
-def find_data_file(filename: str):
+def find_data_file(filename: str) -> str:
     if getattr(sys, "frozen", False):
         # The application is frozen
         datadir = os.path.dirname(sys.executable)
